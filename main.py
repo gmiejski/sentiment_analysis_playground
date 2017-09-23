cfba4ed0292894c0efaf995c7a4215d336bf0490
gmiejski <grz.miejski@gmail.com>
2017-09-23 21:20:50 +0200

    Working analyser for simple example.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import gzip
 from sentiment_tokenize.tokenizer import Tokenizer
+from data_loading import load
+from scipy.sparse import csr_matrix
+from scipy.sparse import coo_matrix, vstack
 
 dataset_file = "data/reviews_Movies_and_TV_5.json.gz"
 
@@ -13,74 +16,57 @@ def parse(path):
 generator = parse(dataset_file)
 tokenizer = Tokenizer()
 
-for a in [x for _, x in zip(range(10), generator)]:
-    # print(a)
-    text = a['reviewText']
-    print(text)
-    s0 = "This is a cooool #dummysmiley: :-) :-P <3 and some arrows < > -> <--"
-    print(tokenizer.tokenize(text))
+dataset = load.load(generator, 100)
+dataset = list(map(lambda entry: (tokenizer.tokenize(entry[0]), entry[1]), dataset))
 
-print()
-print()
-print()
+
+def create_dictionary(dataset):
+    words = set()
+    for entry in dataset:
+        for token in entry[0]:
+            words.add(token)
+    return dict(zip(words, range(len(words))))
+
+
+words_dictionary = create_dictionary(dataset)
 
 from sklearn.naive_bayes import MultinomialNB
 
 sentences = ['Ala ma kota.',
              'Dominik ma psa',
              'Kot nie jest taki zły',
-             'Ala nie może palić'
+             'Ala nie może palić',
+             "Ale Ala nie jest taka głupia."
              ]
+classes = [1, 0, 0, 1, 1]
+
+data = list(map(lambda x: (x[1], classes[x[0]]), zip(range(len(sentences)), sentences)))
+words_dictionary = create_dictionary(data)
 
 
-def feature_array(sentence, tokenizer, words_indexes):
-    tokens = tokenizer.tokenize(sentence)
-    for token in tokens:
-        if token not in words_indexes.keys():
-            words_indexes[token] = len(words_indexes)
-
-    features = [0] * len(words_indexes)
-    for token in tokens:
-        features[words_indexes[token]] = features[words_indexes[token]] + 1
-
-    return features
+def c_cross_validation(data):
+    return data[:4], data[-1]
 
 
-def makeMatrix(sentences):
-    tokenizer = Tokenizer()
-    words_indexes = {}
-    result = []
-
-    for x in sentences:
-        features = feature_array(x, tokenizer, words_indexes)
-        result.append(features)
-
-    for feature_list in result:
-        for empty in range((len(words_indexes) - len(feature_list))):
-            feature_list.append(0)
-
-    return result, words_indexes
+training, test = c_cross_validation(data)
 
 
-X, words_indexes = makeMatrix(sentences)
-test_sentence = "Ale Ala nie jest taka głupia."
-test_X = feature_array(test_sentence, tokenizer, words_indexes)
+def feature_array(entries, words_indexes):
+    matrixes = []
+    for entry in entries:
+        empty = [0] * len(words_indexes)
+        for token in entry[0]:
+            empty[words_indexes[token]] += 1
+        matrix = csr_matrix([empty])
+        matrixes.append(matrix)
+    return vstack(matrixes)
 
-classes = [1, 0, 0, 1]
+
+train_X = feature_array(training, words_dictionary)
+test_X = feature_array([test], words_dictionary)
 
 clf = MultinomialNB()
 
-
-
-def align_features(X, words_indexes):
-    for feature_list in X:
-        for empty in range((len(words_indexes) - len(feature_list))):
-            feature_list.append(0)
-    return X
-
-
-X = align_features(X, words_indexes)
-
-clf.fit(X, classes)
-y = clf.predict([test_X, test_X])
+clf.fit(train_X, classes[:len(training)])
+y = clf.predict(test_X)
 print(y)
